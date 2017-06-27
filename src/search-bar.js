fb7d9b99d23a2221069ef271ec8486b6203b6af0
Mehul Kar <mehul.kar@gmail.com>
2017-06-27 06:12:51 -0700

    WIP of open/closing search bar

--- src/search-bar.js
+++ src/search-bar.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import searchIcon from './images/search.png'
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+    this.showSearchBar = this.showSearchBar.bind(this);
+  }
+
+  showSearchBar() {
+    this.setState({open: !this.state.open})
+  }
+
   render() {
     return (
       <div className='search-bar'>
-        <img src={searchIcon} alt='search' />
+        <img src={searchIcon} alt='search' onClick={this.showSearchBar}/>
+        <span>{this.state.open}</span>
       </div>
     );
   }
