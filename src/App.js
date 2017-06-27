23b7cf4ceb7ada7ab24aadec6d59f98c04da7a98
Mehul Kar <mehul.kar@gmail.com>
2017-06-27 04:56:05 -0700

    Replaces default content with header icons and gradients

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/beaver 1.png';
+import settingsIcon from './images/setting icon.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} alt="logo" />
+          <spacer></spacer>
+          <img src={settingsIcon} alt="logo" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='App-body'>
+          TODO
+        </div>
       </div>
     );
   }
