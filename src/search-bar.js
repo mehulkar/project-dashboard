daecad92e497dc6d084dde0fa46e2c6159687c6c
Mehul Kar <mehul.kar@gmail.com>
2017-06-28 18:23:27 -0700

    Add an input field that appears on demand

--- src/search-bar.js
+++ src/search-bar.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import searchIcon from './images/search.png'
+import './search-bar.css'
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { open: false };
+    this.state = {
+      open: false,
+      searchValue: ''
+    };
     this.showSearchBar = this.showSearchBar.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   showSearchBar() {
     this.setState({open: !this.state.open})
   }
 
+  handleChange(event) {
+    this.setState({searchValue: event.target.value});
+  }
+
   render() {
+    let searchBar = null;
+    if (this.state.open) {
+      searchBar = <input value={this.state.searchValue}
+                        placeholder='Search for a project...'
+                        onChange={this.handleChange} />
+    }
     return (
       <div className='search-bar'>
         <img src={searchIcon} alt='search' onClick={this.showSearchBar}/>
-        <span>{this.state.open}</span>
+        {searchBar}
       </div>
     );
   }
